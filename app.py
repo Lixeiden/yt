caa406e9e3449a13a480ce2d66da7d54f1af281d
Lixeiden <lixeiden@gmail.com>
2021-10-14 21:31:02 +0200

    app name changed

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template, request
 import script
 
-flask1 = Flask(__name__)
+app = Flask(__name__)
 
 
-@flask1.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST' and request.form['videoUrl']:
         script.Download(videoUrl=request.form['videoUrl'], format=request.form['selectFormat'])
     return render_template('index.html')
 
 
-@flask1.route('/purge', methods=['POST', 'GET'])
+@app.route('/purge', methods=['POST', 'GET'])
 def purge():
     if request.method == 'POST' and request.form['selectFile'] != 'default':
         # del files here
@@ -19,10 +19,10 @@ def purge():
     return render_template('purge.html')
 
 
-@flask1.route('/list')
+@app.route('/list')
 def list():
     return render_template('list.html')
 
 
 if __name__ == '__main__':
-    flask1.run(debug=True)
+    app.run(debug=True)

2a4f4eb335d7aa41599231b134ca25bbb7641d12
Lixeiden <lixeiden@gmail.com>
2021-10-16 03:52:32 +0200

    run gunicorn with gthread worker

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -1,2 +1,2 @@
 source ./venv/bin/activate
-gunicorn -w 3 -b 127.0.0.1:8002 'app:create_app(testing=False)'
+gunicorn --workers=3 --threads=2 --worker-class=gthread --bind=127.0.0.1:8002 'app:create_app(testing=False)'

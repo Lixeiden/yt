79e8155b5cdb916aac971785ba84fb90eef713a2
Lixeiden <lixeiden@gmail.com>
2023-02-21 16:06:39 +0200

    audio: bestaudio instead of 140

--- script.py
+++ script.py
@@ -16,7 +16,7 @@ def Download(videoUrl, format):
 
     mode = {
         'best': ['bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio', '--merge-output-format', 'mp4'],
-        'audio': ['140', ]
+        'audio': ['bestaudio', ]
     }
 
     with open(f'./logs/{videoUrl[-11:]}_log.txt', 'a') as logFile:

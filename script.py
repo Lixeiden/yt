5541d3e99ca937b28e3b4deeaca98bacd92456e5
Lixeiden <lixeiden@gmail.com>
2023-02-22 11:45:25 +0200

    refactor format bestaudio

--- script.py
+++ script.py
@@ -16,8 +16,8 @@ def Download(videoUrl, format):
 
     mode = {
         'best': ['bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio', '--merge-output-format', 'mp4'],
-        'audio': ['bestaudio', ]
+        'audio': ['--format', 'bestaudio']
     }
 
     with open(f'./logs/{videoUrl[-11:]}_log.txt', 'a') as logFile:
-        subprocess.run(['yt-dlp', '--format', *mode[format], '--output', './videos/%(uploader)s-%(title)s-%(id)s.%(ext)s', videoUrl], stdout=logFile, stderr=logFile)
+        subprocess.run(['yt-dlp', *mode[format], '--output', './videos/%(uploader)s-%(title)s-%(id)s.%(ext)s', videoUrl], stdout=logFile, stderr=logFile)
